58e150655a54dfe5a5b5fbef1f74ed3654fac4ad
gunjuzone <Gunjuzone@gmail.com>
2021-08-09 19:25:39 +0100

    Select Inputs and Add button

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('Connected');
+// Book Class: Represents books
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+const addBtn = document.querySelector('.add-btn');
+const bookUl = document.querySelector('.book-ul');
+
+//  UI Class: Handle Dynamica book
+// Local Storage : Store books
+// Events: Events to display
+// Events: Events to add a book
+// Events to remove a book
