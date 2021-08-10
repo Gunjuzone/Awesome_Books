c0161548921d879fa204cf7582bf2b7015474f0e
gunjuzone <Gunjuzone@gmail.com>
2021-08-10 23:25:16 +0100

    Add book dynamically

--- scripts/app.js
+++ scripts/app.js
@@ -2,36 +2,64 @@ const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const addBtn = document.querySelector('.add-btn');
 const formBooks = document.querySelector('#form');
-const bookList = document.querySelector('.book-list');
+const bookSection = document.querySelector('.book-list');
 
 const books = (title, author) => ({
-  title, author
-})
+  title,
+  author,
+});
 
 const booksList = [];
 
-
 const saveBook = (list) => {
   booksList.push(list);
-}
+};
 
 const clearfields = () => {
   titleInput.value = '';
   authorInput.value = '';
-}
+};
 
-formBooks.addEventListener('submit', (e) =>{
+formBooks.addEventListener('submit', (e) => {
   e.preventDefault();
   const titleValue = titleInput.value;
   const authorValue = authorInput.value;
-  const list = books(titleValue, authorValue)
+  const list = books(titleValue, authorValue);
   saveBook(list);
   clearfields();
-})
+});
 
+console.log(booksList);
 
+// Add Book Dynamically
 
-console.log (booksList);
+const createBookElement = (book) => {
+  const bookContainer = document.createElement('div');
+  bookContainer.innerHTML = `
+    <p class="book-title">${book.title}</p>
+    <p class="book-author">${book.author}</p>
+    <button type="button" data-action="delete" data-book-id=" class="remove-btn">Remove</button>
+    `;
+
+  return bookContainer;
+};
+const displayBook = (book, bookSection) => {
+  const newBookElement = createBookElement(book);
+  bookSection.appendChild(newBookElement);
+};
+
+booksList.forEach((book) => {
+  displayBook(book, bookSection);
+});
+
+// const bookContainer = document.createElement('div');
+// bookContainer.innerHTML = `
+//     <p class="book-title">${book.title}</p>
+//     <p class="book-author">${book.author}</p>
+//     <button type="button" data-action="delete" class="remove-btn" >Remove</button>
+//     `;
+
+// bookList.appendChild(bookContainer);
 
 // form.addEventListener('submit', (e) => {
 //   e.preventDefault();
@@ -55,12 +83,4 @@ console.log (booksList);
 //   });
 // });
 //
-// function deleteButton(remove, index) {}
-// const firstBook = new awesomeBook('The Stranger', 'Albert Camus');
-// const secondBook = new awesomeBook('Myth of Sisyphus', 'Albert Camus');
-
-// let bookArray = [firstBook, secondBook];
-
-// const bookCollection = document.querySelector('.book-list');
-
-// const createBookElement = (book) => {};
+//
