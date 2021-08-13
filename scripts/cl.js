2d585e8e92d1a18cfd3992be92e39c4d848e04a2
Leonardo <fanger53@gmail.com>
2021-08-13 00:01:49 -0500

    changing index.html for add new features

--- scripts/cl.js
+++ scripts/cl.js
@@ -43,3 +43,27 @@ class Store {
   }
 }
 
+
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const nPages = document.querySelector('#nPages').value;
+
+  if (title === '' || author === '' || nPages === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    const book = new Book(title, author, nPages);
+    UI.addBookToList(book);
+    Store.addBook(book);
+    UI.showAlert('Book Added', 'success');
+    UI.clearFields();
+  }
+});
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+});
