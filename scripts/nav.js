84ed8a631ef07cc04778ce490784f9121dc248bb
Leonardo <fanger53@gmail.com>
2021-08-13 16:30:27 -0500

    add nav bar

---
+++ scripts/nav.js
@@ -0,0 +1,5 @@
+const listButton = document.querySelector('.list-button');
+const contactsButton = document.querySelector('.add-contacts');
+const addNewButton = document.querySelector('.add-new');
+const homeButton = document.querySelector('.home');
+
