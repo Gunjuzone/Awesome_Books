8b928ce9485c0a0b597d5e068d1ac55f4664c44a
Leonardo <fanger53@gmail.com>
2021-08-13 00:03:36 -0500

    add UI class

--- scripts/cl.js
+++ scripts/cl.js
@@ -43,7 +43,50 @@ class Store {
   }
 }
 
+class UI {
+  static displayBooks() {
+    const StoredBooks = Store.getBooks();
+    const books = StoredBooks;
+    books.forEach((book) => UI.addBookToList(book));
+  }
 
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.nPages}</td>
+      <td class="has-text-centered"><button  id=${book.id} value="Read">Read</button></td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+    `;
+    list.appendChild(row);
+    const td = document.getElementById(book.id);
+    td.addEventListener('click', (e) => change(e));
+  }
+
+  static deleteBook(element) {
+    if (element.classList.contains('delete')) {
+      element.parentElement.parentElement.remove();
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.form_box');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#nPages').value = '';
+  }
+}
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
