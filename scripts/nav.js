d89a022afb8df4e7a6b991d690cc980605892a2a
Leonardo <fanger53@gmail.com>
2021-08-13 16:49:37 -0500

    add functionality new book and contact btn, also footer added too

--- scripts/nav.js
+++ scripts/nav.js
@@ -8,7 +8,7 @@ const form = document.querySelector('.form-section');
 
 homeButton.addEventListener('click', (e) => {
   e.preventDefault();
-  contact.classList.remove('d-none');
+  contact.classList.add('d-none');
   list.classList.remove('d-none');
   form.classList.remove('d-none');
   addNewButton.classList.remove('active');
@@ -24,4 +24,24 @@ listButton.addEventListener('click', (e) => {
   listButton.classList.add('active');
   addNewButton.classList.remove('active');
   contactsButton.classList.remove('active');
+});
+
+addNewButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  contact.classList.add('d-none');
+  list.classList.add('d-none');
+  form.classList.remove('d-none');
+  addNewButton.classList.add('active');
+  listButton.classList.remove('active');
+  contactsButton.classList.remove('active');
+});
+
+contactsButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  list.classList.add('d-none');
+  form.classList.add('d-none');
+  contact.classList.remove('d-none');
+  contactsButton.classList.toggle('active');
+  addNewButton.classList.remove('active');
+  listButton.classList.remove('active');
 });
